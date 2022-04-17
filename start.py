20491253e5bae5d017366009c428e51a53dbe80d
karlsheng99 <karlsheng99@gmail.com>
2022-04-17 18:05:19 -0400

    bug fixed

--- start.py
+++ start.py
@@ -13,9 +13,8 @@ timeout = 5
 while True:
     try:
         request = requests.get(url, timeout=timeout)
-        ssid = subprocess.check_output(['sudo', 'iwgetid'])
-        #lcd.display(lcd1602, 'WiFi Connected', ssid.split('"')[1], (144,249,15))
-        lcd.display(lcd1602, 'WiFi Connected', 'TP-Link_057B', (144,249,15))
+        ssid = str(subprocess.check_output(['sudo', 'iwgetid']))
+        lcd.display(lcd1602, 'WiFi Connected', ssid.split('"')[1], (144,249,15))
     except(requests.ConnectionError, requests.Timeout) as exception:
         lcd.display(lcd1602, 'No Internet', '', (255,0,0))
 
