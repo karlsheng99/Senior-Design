ee488fd3256a6859c7e9e1bc109c8530d7202e44
karlsheng99 <karlsheng99@gmail.com>
2022-04-17 19:02:08 -0400

    updated startup program

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
+from operator import truediv
 from requests import request
 import requests
 import RGB1602
 import lcd
 import subprocess
 import time
+import RPI.GPIO as GPIO
+import os
+import signal
 
 lcd1602 = RGB1602.RGB1602(16, 2)
 
@@ -15,7 +19,38 @@ while True:
         request = requests.get(url, timeout=timeout)
         ssid = str(subprocess.check_output(['sudo', 'iwgetid']))
         lcd.display(lcd1602, 'WiFi Connected', ssid.split('"')[1], (144,249,15))
+        break
     except(requests.ConnectionError, requests.Timeout) as exception:
         lcd.display(lcd1602, 'No Internet', '', (255,0,0))
+        time.sleep(1)
 
-    time.sleep(1)
+b1 = 37
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(b1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+program_state = False
+main_pid = 0
+server_pid = 0
+lt_pid = 0
+
+while True:
+    button_state = GPIO.input(b1)
+
+    # run bash script
+    if button_state == False and program_state == False:
+        main = subprocess.Popen(['python', 'main.py'])
+        main_pid = main.pid
+        server = subprocess.Popen(['python', 'manage.py', 'runserver', '0:8000'])
+        server_pid = server.pid
+        lt = subprocess.Popen(['lt', '-p', '8000', '-s', 'tasktop'])
+        lt_pid = lt.pid
+
+        program_state = True
+
+    # send interrupt signal
+    elif button_state == False and program_state == True:
+        os.kill(main_pid, signal.SIGTERM)
+        os.kill(server_pid, signal.SIGTERM)
+        os.kill(lt_pid, signal.SIGTERM)
+        
+        program_state = False
