7b7e699b7d491b63e7cdec8a466ac967f28d4b8b
karlsheng99 <karlsheng99@gmail.com>
2022-04-17 17:33:12 -0400

    test

--- start.py
+++ start.py
@@ -14,8 +14,9 @@ while True:
     try:
         request = requests.get(url, timeout=timeout)
         ssid = subprocess.check_output(['sudo', 'iwgetid'])
-        lcd.display(lcd1602, "WiFi Connected", ssid.split('"')[1], (144,249,15))
+        #lcd.display(lcd1602, 'WiFi Connected', ssid.split('"')[1], (144,249,15))
+        lcd.display(lcd1602, 'WiFi Connected', 'TP-Link-057B', (144,249,15))
     except(requests.ConnectionError, requests.Timeout) as exception:
-        lcd.display(lcd1602, "No Internet", "", (255,0,0))
+        lcd.display(lcd1602, 'No Internet', '', (255,0,0))
 
     time.sleep(1)
