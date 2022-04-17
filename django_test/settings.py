0d530b7b34fe575df1fd36adaa518418703a5a3f
karlsheng99 <karlsheng99@gmail.com>
2022-04-17 03:04:19 -0400

    updated settings

--- django_test/settings.py
+++ django_test/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from email import message
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -124,3 +126,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
